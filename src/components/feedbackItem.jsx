26291deee194e7a81f16c6d0b65c3d320d89c632
Chandan Kumar <chandanofficial.cn@gmail.com>
2022-01-21 20:08:36 +0530

    1

--- src/components/feedbackItem.jsx
+++ src/components/feedbackItem.jsx
@@ -2,12 +2,19 @@ import { useState} from 'react'
 
 function FeedbackItem() {
     const [rating, setRating] = useState(8)
+    const [review, setReview] = useState("This is a Feedback text testing !")
+
+    const handleClick = () =>{
+        setRating(9)
+    }
+
   return (
     <div className="card">
         <div className="num-display">{rating}</div>
         <div className="text-display">
-            This is a Feedback text testing !
+            {review}
         </div>
+        <button onClick={handleClick}>Click</button>
     </div>
   )
 }
